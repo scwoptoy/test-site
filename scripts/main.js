12cc2078d7ee043e725c12a972c868af5278596b
John McCarley <scwoptoy@gmail.com>
2019-12-09 16:04:45 -0500

    added an image changer

--- scripts/main.js
+++ scripts/main.js
@@ -6,5 +6,13 @@ All text between forward slash and asterisk is considered a comment.
 Use this comment method whenever you need line breaks.
 */
 
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
