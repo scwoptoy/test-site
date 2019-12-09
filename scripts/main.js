ec7f703a128c627def144e93339cf2d4bac532e3
John McCarley <scwoptoy@gmail.com>
2019-12-09 11:07:21 -0500

    added comments to javascript file and commited previous changes to index

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
+
+// Change Heading 1 to say Hello World - two forward slashes denote a comment
+
+/*
+All text between forward slash and asterisk is considered a comment. 
+Use this comment method whenever you need line breaks.
+*/
+
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
